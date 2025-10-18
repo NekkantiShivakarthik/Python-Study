812c6a9d76d3f00d026716f1c34cf72769c877c7
Nekkanti Shiva karthik <nekkantishivakarthik25ci@rnsit.ac.in>
2025-10-18 17:57:42 +0530

    Add initial Python scripts for basic output and area calculation

---
+++ number.py
@@ -0,0 +1,6 @@
+length = int(input("Enter the length"))
+breadth= int(input("Enter the breadth"))
+
+area=length*breadth
+
+print("The area of the rectangle is ",area)
---
+++ variables.py
@@ -0,0 +1,5 @@
+message="Hello world"
+print(message.lower())
+print(message.upper())
+print(message.swapcase())
+
